5cc20cd0ac531cdb026c40e23d2cb2b318209a5a
Jacob Lacsamana <jacoblacsamana0@gmail.com>
2024-06-02 15:45:22 -0700

    add readme & licenses for dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("org.nanohttpd:nanohttpd:2.3.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
